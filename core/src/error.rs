abffd31506cf515463c995c886b63949de7c625d
billythedummy <dhy1996@live.com.sg>
2025-06-12 20:02:52 +0800

    impl error

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,5 +1,16 @@
-#[derive(Debug)]
+use core::{error::Error, fmt::Display};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum ReserveError {
     NotEnoughLiquidity,
     InternalError,
 }
+
+impl Display for ReserveError {
+    // Display=Debug, since this is just a simple str enum
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl Error for ReserveError {}
